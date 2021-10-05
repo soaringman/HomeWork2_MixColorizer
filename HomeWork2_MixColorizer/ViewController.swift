6ac1c3995d8873a74a323c4771db120778000b9a
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-10-05 16:14:19 +0300

    Add methods from set Color set Text and add tags from slider and labels

--- HomeWork2_MixColorizer/ViewController.swift
+++ HomeWork2_MixColorizer/ViewController.swift
@@ -28,10 +28,7 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
-        resultView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                             green: CGFloat(greenSlider.value),
-                                             blue: CGFloat(blueSlider.value),
-                                             alpha: 1)
+        setColor()
         
         redLabel.text = String(format: "%.1f", redSlider.value)
         greenLabel.text = String(format: "%.1f", greenSlider.value)
@@ -40,14 +37,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rgbSlider(_ sender: UISlider) {
-        resultView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                             green: CGFloat(greenSlider.value),
-                                             blue: CGFloat(blueSlider.value),
-                                             alpha: 1)
+        setColor()
 //
         redLabel.text = String(format: "%.1f", redSlider.value)
         greenLabel.text = String(format: "%.1f", greenSlider.value)
         blueLabel.text = String(format: "%.1f", blueSlider.value)
+    }
+    
+    // MARK: Methods
+    private func setColor() {
+        resultView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                             green: CGFloat(greenSlider.value),
+                                             blue: CGFloat(blueSlider.value),
+                                             alpha: 1)
+    }
+        
+        private func string(from slider: UISlider) -> String {
+            String(format: "%.1f", slider.value)
     }
 }
 
